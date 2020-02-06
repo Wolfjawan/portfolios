718109a3356277c268e27d206d7b25b93534e428
Wolfjawan <mohsen000069@gmail.com>
2020-02-06 02:22:12 +0000

    add text editor project

--- src/Routs/index.js
+++ src/Routs/index.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "../Screens/Home";
-import Resume from '../Screens/Resume'
+import Resume from "../Screens/Resume";
 import NotFound from "../Components/NotFound";
-
+import MoDraftJS from "../projects/modraftjs";
 class Routs extends Component {
   render() {
     return (
@@ -12,6 +12,7 @@ class Routs extends Component {
         <Switch>
           <Layout exact path="/" component={Home} {...this.props} />
           <Route exact path="/resume" component={Resume} />
+          <Route exact path="/modraftjs" component={MoDraftJS} />
           <Route component={NotFound} />
         </Switch>
       </Router>
